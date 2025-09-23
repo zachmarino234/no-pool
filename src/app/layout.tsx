6b312442038736f36a34e3cac5e43e0a3da8f240
zmar234 <zachmarino234@gmail.com>
2025-09-23 15:35:33 -0400

    layout.tsx cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import NavBar from "@/components/NavBar";
 import "./globals.css";
 import NavBear from "@/components/NavBear";
 import Footer from "@/components/Footer";
